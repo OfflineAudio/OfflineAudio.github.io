636c4faae8e392a4432609815c5de895af3e137f
Jake Champion <jakechampion.jake2@googlemail.com>
2015-02-13 00:24:41 +0000

    Tidy up

--- js/actions/LibraryActions.js
+++ js/actions/LibraryActions.js
@@ -1,31 +1,40 @@
-var AppDispatcher = require('../dispatcher/AppDispatcher')
-var LibraryConstants = require('../constants/LibraryConstants')
-var Library = require('../utils/Library')
+let AppDispatcher = require('../dispatcher/AppDispatcher')
+let LibraryConstants = require('../constants/LibraryConstants')
+let Library = require('../utils/Library')
 
-var LibraryActions = {
+function createLibrary (docs) {
+  return docs.reduce(function(library, b) {
+    let artist = library[b.artist] || (library[b.artist] = Object.create(null))
+    let album = artist[b.album] || (artist[b.album] = Object.create(null))
+    let track = album[b.title] || (album[b.title] = Object.create(null))
+    track['id'] = b._id
+    track['rev'] = b._rev
+    return library
+  }, Object.create(null))
+}
+
+function pluckDocs (data) {
+  return data.rows.map(row => row.doc)
+}
+
+function dispatchSuccessUpdate(library) {
+  AppDispatcher.handleAction({
+    actionType: LibraryConstants.LIBRARY_UPDATE_SUCCESS,
+    data: library
+  })
+}
+
+function dispatchErrorUpdate(err) {
+  console.error(err)
+}
+
+let LibraryActions = {
   update: function() {
     Library.read()
-    .then(function(data) {
-      data = data.rows.map(function(row) {
-        return row.doc
-      })
-      .reduce(function(library, b) {
-        var artist = library[b.artist] || (library[b.artist] = Object.create(null))
-        var album = artist[b.album] || (artist[b.album] = Object.create(null))
-        var track = album[b.title] || (album[b.title] = Object.create(null))
-        track['id'] = b._id
-        track['rev'] = b._rev
-        return library
-      }, Object.create(null))
-
-      AppDispatcher.handleAction({
-        actionType: LibraryConstants.LIBRARY_UPDATE_SUCCESS,
-        data: data
-      })
-    })
-    .catch(function(err) {
-      console.error(err)
-    })
+    .then(pluckDocs)
+    .then(createLibrary)
+    .then(dispatchSuccessUpdate)
+    .catch(dispatchErrorUpdate)
 
     AppDispatcher.handleAction({
       actionType: LibraryConstants.LIBRARY_UPDATE,
